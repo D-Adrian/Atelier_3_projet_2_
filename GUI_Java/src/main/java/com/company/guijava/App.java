99777b78cd02b8cf51c499e1d823dfa0aed54527
Eddy <eddy.marquand@outlook.fr>
2019-12-10 14:26:29 +0100

    add dependencies and connexion message with command

--- GUI_Java/src/main/java/com/company/guijava/App.java
+++ GUI_Java/src/main/java/com/company/guijava/App.java
@@ -1,8 +1,19 @@
 package com.company.guijava;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class App {
 
-    public static void main(String[] args) {
+    public static void main(String[] args)  {
+
+        try {
+            Connection connection = DriverManager.getConnection("jdbc:mysql://172.20.10.7:3306/BaseDeDonnee", "Eddy", "shayze7895");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Connected to Database");
 
     }
 }
