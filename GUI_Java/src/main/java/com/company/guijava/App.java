8ccf0fc8b870adb46b8d5dd636762140db725dc9
Eddy <eddy.marquand@outlook.fr>
2019-12-10 14:48:51 +0100

    m

--- GUI_Java/src/main/java/com/company/guijava/App.java
+++ GUI_Java/src/main/java/com/company/guijava/App.java
@@ -5,15 +5,13 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class App {
-
     public static void main(String[] args) {
 
         try {
-            Connection connection = DriverManager.getConnection("jdbc:mysql://172.20.10.7:3306/BaseDeDonnee", "Eddy", "shayze7895");
+            Connection connection = DriverManager.getConnection("jdbc:mysql://172.20.20.7:3306/BaseDeDonnee", "Eddy", "shayze7895");
         } catch (SQLException e) {
             e.printStackTrace();
         }
-        System.out.println("Connected to Database");
 
     }
 }
